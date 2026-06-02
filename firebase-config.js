09fc019c54d0171974c4051624811f3183a56d1b
komacloths-ship-it <komacloths@gmail.com>
2026-06-02 19:38:26 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.KOMA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAtp1D3yPkZeU0bsJai6UMTq3OV8PvPjWQ",
-  authDomain: "koma-b7aeb.firebaseapp.com",
-  projectId: "koma-b7aeb",
-  storageBucket: "koma-b7aeb.firebasestorage.app",
-  messagingSenderId: "980121644350",
-  appId: "1:980121644350:web:daa8b2792abb0bf859506f",
-  measurementId: "G-KC7XRT2PVV",
-};
